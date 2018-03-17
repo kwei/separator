1ad1ec9dbb2d361b5cc81c62830d18f9c7120516
葉鎧瑋 <a0979597291@gmail.com>
2018-03-17 14:22:40 +0800

    Add files via upload

--- separator.py
+++ separator.py
@@ -1,38 +1,7 @@
 import csv
-import os
 import datetime
 import time
 from colorama import Fore, Back, Style
-import tkinter as tk
-
-os.system('clear')
-
-oldfilename = input("Type the file name with path you want to separate.\n> ")
-newfilename = input("Type the new file name with path you want to save.\n> ")
-'''
-mainWin = tk.Tk()
-mainWin.title("CSV Separator")
-mainWin.geometry('200x100')
-
-filenameLabel = tk.Label(mainWin,text="Type the file name with path you want to separate.")
-filenameLabel.pack()
-filenameEntry = tk.Entry(mainWin,text="input")
-filenameEntry.pack()
-
-newfilenameLabel = tk.Label(mainWin,text="Type the new file name with path you want to save.")
-newfilenameLabel.pack()
-newfilenameEntry = tk.Entry(mainWin,text="input")
-newfilenameEntry.pack()
-'''
-station = []
-head = []
-'''
-oldfilename = filenameEntry.get()
-print("[ " + Fore.GREEN + "Info" + Fore.RESET + " ] " + "old file name : " + oldfilename)
-newfilename = newfilenameEntry.get()
-print("[ " + Fore.GREEN + "Info" + Fore.RESET + " ] " + "new file name : " + newfilename)
-'''
-
 class Dataset:
 	def __init__(self):
 		self.__head = []
@@ -47,56 +16,49 @@ class Dataset:
 			i = []
 			self.createone(i)
 		return self.__head
-
-begin = datetime.datetime.now()
-with open(oldfilename) as csvfile:
-	csvfile.seek(0)
-	readcsv = csv.reader(csvfile, delimiter=',')
-	iterator = 1
-
-	for row in readcsv:
-		iterator += 1
-		temp = ''
-		
-		for matched in station:
-			if matched == row[0]:
-				temp = matched
-				break
-		if temp != row[0]:
-			station.append(row[0])
-
-	print("[ " + Fore.GREEN + "Info" + Fore.RESET + " ] " + "Loading files and setting up the service...")
-	mydata = Dataset()
-	mydata.__init__()
-	#time.sleep(2)
-	head = mydata.auto_create(len(station))
-
-	for count in range(len(station)):
+iscontinue = 1
+while iscontinue == 1:
+	oldfilename = input("Type the file name with path you want to separate.\n> ")
+	newfilename = input("Type the new file name with path you want to save.\n> ")
+	station = []
+	head = []
+	begin = datetime.datetime.now()
+	with open(oldfilename) as csvfile:
 		csvfile.seek(0)
+		readcsv = csv.reader(csvfile, delimiter=',')
+		iterator = 1
 		for row in readcsv:
-			if row[0] == station[count]:
-				sublist = head[count]
-				sublist.append(row)
-		count += 1
-
-	print("[ " + Fore.GREEN + "Info" + Fore.RESET + " ] " + "Total data's amounts is " + str(iterator))
-	csvfile.close()
-
-with open(newfilename, 'w') as csvfile:
-	csvfile.seek(0)
-	writecsv = csv.writer(csvfile, delimiter=',')
-	print("[ " + Fore.GREEN + "Info" + Fore.RESET + " ] " + "Start to rewrite the datas!")
-	#time.sleep(2)
-
-	for first in range(len(station)):
-		for second in range(len(head[first])):
-			sta = head[first]
-			writecsv.writerow(sta[second])
-
-	csvfile.close()
-
-end = datetime.datetime.now()
-total_time_cost = end.second - begin.second
-print("[ " + Fore.GREEN + "Info" + Fore.RESET + " ] " + "Total time cost = " + str(total_time_cost) + " s")
-
-#mainWin.mainloop()
+			iterator += 1
+			temp = ''
+			for matched in station:
+				if matched == row[0]:
+					temp = matched
+					break
+			if temp != row[0]:
+				station.append(row[0])
+		print("[ " + Fore.GREEN + "Info" + Fore.RESET + " ] " + "Loading files and setting up the service...")
+		mydata = Dataset()
+		mydata.__init__()
+		head = mydata.auto_create(len(station))
+		for count in range(len(station)):
+			csvfile.seek(0)
+			for row in readcsv:
+				if row[0] == station[count]:
+					sublist = head[count]
+					sublist.append(row)
+			count += 1
+		print("[ " + Fore.GREEN + "Info" + Fore.RESET + " ] " + "Total data's amounts is " + str(iterator))
+		csvfile.close()
+	with open(newfilename, 'w') as csvfile:
+		csvfile.seek(0)
+		writecsv = csv.writer(csvfile, delimiter=',')
+		print("[ " + Fore.GREEN + "Info" + Fore.RESET + " ] " + "Start to rewrite the datas!")
+		for first in range(len(station)):
+			for second in range(len(head[first])):
+				sta = head[first]
+				writecsv.writerow(sta[second])
+		csvfile.close()
+	end = datetime.datetime.now()
+	total_time_cost = end.second - begin.second
+	print("[ " + Fore.GREEN + "Info" + Fore.RESET + " ] " + "Total time cost = " + str(total_time_cost) + " s")
+	iscontinue = input("Continue to convert csv file? (yes: 1 ； no : 0)\n> ")
